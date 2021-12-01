e081691b74516c35e55f14865576f4f2f580090e
Spencer Talbot <stalbot@umich.edu>
2021-11-30 21:15:00 -0500

    changed .mat file variable to have correct name

--- ROB535_ControlProject_part2_Team4.m
+++ ROB535_ControlProject_part2_Team4.m
@@ -1,3 +1,6 @@
 %% Self Driving Cars Control Part 2
 % worked on by Dyllian Powell, Spencer Talbot, and Joseph Drockton
 % This file is empty currently :]
+
+ROB535_ControlProject_part1_input = 0;
+save ROB535_ControlProject_part1_Team4.mat ROB535_ControlProject_part1_input
