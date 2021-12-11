0c1578c849526506b16d1d275eea04606bda90b4
dyllp <80712947+dyllp@users.noreply.github.com>
2021-12-11 16:10:28 -0500

    Added obstacle avoidance logic, need to tune gains
    super janky setup. Comment out line 65 to run without obstacles and vice versa. Dont @ me

--- ROB535_ControlProject_part2_Team4.m
+++ ROB535_ControlProject_part2_Team4.m
@@ -1,6 +1,5 @@
 %% Self Driving Cars Control Part 2
 % worked on by Dyllian Powell, Spencer Talbot, and Joseph Drockton
-% This file is empty currently :]
 
 close all 
 clear all
@@ -12,6 +11,10 @@ center = TestTrack.cline;
 right = TestTrack.br;   % defining right boundary of track
 left = TestTrack.bl;    % defining left boundary of track
 theta = TestTrack.theta; % defining heading angle of track
+
+%% Generating random obstacles (will probably have to remove before submission)
+Nobs = 15;
+Xobs = generateRandomObstacles(Nobs,TestTrack);
 
 %% Finding relative angles of the track and low passing them
 dtheta = ang_diff(theta); % unfortunately we can't use Matlab's angdiff b/c it's not in one of the allowed toolboxes
@@ -59,6 +62,8 @@ accum_error = 0; % accumulated error? if so, I'm not sure what it's for
 
 %% Running the model and performing calculations
 for i=1:loopsteps
+    %[testinput,accum_error] = steering_obs(lpinp,TestTrack,Yprev,testinput,outputsteps,accum_error, Xobs);
+    %steering with obs enabled 
     [testinput,accum_error] = steering(lpinp,TestTrack,Yprev,testinput,outputsteps,accum_error); % calling steering function
     [Y, T]=forwardIntegrateControlInput(testinput(:,1:2),Yprev); % new state calculation
     Y = Y'; % transposing state vector
@@ -77,6 +82,7 @@ plot(right(1,:),right(2,:),'r'); % plotting right boundary
 plot(left(1,:),left(2,:),'r'); % plotting left boundary
 plot(Ystore(1,:),Ystore(3,:),'k'); % plotting car path taken
 gscatter(Ystore(1,:),Ystore(3,:),Inputstore(3,:)); % plotting rules by color for steering
+draw_obs(Xobs)
 title('Vehicle State Colored by turning decision rule'); % setting plot title
 
 %% Plotting path with forward velocity as colored
@@ -93,41 +99,129 @@ title('Center Line of Track');
 
 %% Testing to see if we finish the track using GSI code
 % Yfin = [Ystore(1,:); Ystore(3,:)];
-Yfin = [Ystore(1,:); Ystore(3,:)];
-info = getTrajectoryInfo(Yfin',Inputstore(1:2,:)',[],T,TestTrack); % call to see if we crash
+%Yfin = [Ystore(1,:); Ystore(3,:)];
+%info = getTrajectoryInfo(Yfin',Inputstore(1:2,:)',[],T,TestTrack); % call to see if we crash
 
 %% Functions
-function [move, accumlated_poserr] = steering(lpinp, ref_track, curr_state,lastoutput,outputsteps,accumlated_poserr)
-    
-    maxPinput = 0.07;
-    maxsteeringout = .15;
-    maxsteeringRateOfChange = .07;
-    max_accumerrorlimit = 5;
-    Igain = -.00001;
-    desire_speed = 5;
-    pGainoffCenter = .1;
-    Pdeadband = 1;
+function draw_obs(Xobs)
+    for i = 1:length(Xobs)
+        points = Xobs{i};
+        x_vals = [points(:,1); points(1,1)];
+        y_vals = [points(:,2); points(1,2)];
+
+        plot(x_vals, y_vals, 'b')
+    end
+end
+function [move, accumlated_poserr] = steering_obs(lpinp, ref_track, curr_state,lastoutput,outputsteps,accumlated_poserr,Xobs)
+ maxPinput = 0.07;
+    maxsteeringout = .15; % delta steering
+    maxsteeringRateOfChange = .07;  % derivative clamp 
+    max_accumerrorlimit = 5; % integral error limit
+    Igain = -.00001; %integral gain
+    desire_speed = 5; % Desired Fx steady state speed 
+    pGainoffCenter = .1; % P gain 
+    Pdeadband = 1; % deadbands around line (0 = line exactly pretty much)
     Ideadband = 1;
     output = zeros(outputsteps,3);
     output(:,2) = 90;
 
     curr_state = curr_state';
     cenpts = ref_track.cline;
+    right = ref_track.br;
+    left = ref_track.bl;
+    
+    % construct centerlines for left and right lane
+    c_right = (right + cenpts)/2;
+    c_left = (left+cenpts)/2;
     ref_heading = ref_track.theta;
     curr_pt = [curr_state(1), curr_state(3)];
-
-    %Find closest position on track to current location
-    Idx = knnsearch(cenpts', curr_pt);
-    %look up change in heading using same index
-    matchpt = lpinp(1, Idx);
     
-    poseerror = ang_diff(curr_state(4),ref_heading(Idx));
-    cenpts = cenpts';
+    % check if we see any obstacles (stores in cell array)
+    Xobs_seen = senseObstacles(curr_pt, Xobs);
+    %Obs = zeros(size(Xobs_seen));
     
-    %compute which side of center using cross product and
-    %apply proportional steering correction
-    whichside = whichwayoffcenter(Idx, cenpts, curr_state);
-    refpt = [cenpts(Idx,1) cenpts(Idx,2)];
+    % calculate center point of each obstacle
+    Obs = [];
+    for i=1:size(Xobs_seen)
+        Obs(i,:) = mean(Xobs_seen{i});
+    end
+    front_obs =[];
+    % extract heading angle from vehicle and relative angle to obstacle
+    % center point
+    heading = curr_state(5);
+    % calculate distance between car and obstacles
+    if(Obs)
+        for i=1:size(Obs)
+           obx = Obs(i,1); % x location of obs
+           oby = Obs(i,2); % y location
+           x_d = obx - curr_pt(1); % x distance of triangle
+           y_d = oby - curr_pt(2); % y distance of triangle
+           theta = atan(x_d/y_d); % angle of vector to obstacle from x axis
+           alpha = abs(heading - theta); % angle between heading and obstacle
+           dist = norm(curr_pt - [obx,oby]);
+           obs_vec = [dist,obx,oby,x_d,y_d,alpha];
+           if (alpha < deg2rad(90))
+              front_obs = [front_obs;obs_vec]; 
+           end
+        end
+    end
+    % final front_obs vector: [distance, x,y, x_d, y_d, alpha] 
+    % find closest obstacle
+    if(front_obs)
+        [~,I] = min(front_obs(:,1));
+        %grab global x,y of next closest obstacle
+        nxt_obs = front_obs(I,2:3);
+        %WANT TO: calculate which side obstacle is on (L,R), then tell car to
+        %go to follow the lane oposite to that side.
+        [~,l_point] = knnsearch(left',nxt_obs); % nearest point to obstacle on either side
+        [~,r_point] = knnsearch(right',nxt_obs);
+        %side_l = norm(l_point - nxt_obs); % distance between obstacle and track sides
+        %side_r = norm(r_point - nxt_obs);
+        
+        if (l_point < r_point) % obstacle closer to left side of track
+            % set new goal path to right lane
+            disp('Changing lane to right lane @\n')
+            disp(curr_pt)
+            lane = c_right';
+        else % else obstacle closer to right side
+            % set new goal path to left lane
+            disp('Changing lane to left lane @\n')
+            disp(curr_pt)
+            lane = c_left';
+        end
+        Idx = knnsearch(lane, curr_pt);
+        %look up change in heading using same index
+        matchpt = lpinp(1, Idx);
+        %poseerror = angdiff(curr_state(4),ref_heading(Idx));
+     
+        %compute which side of center using cross product and
+        %apply proportional steering correction
+        whichside = whichwayoffcenter(Idx, lane, curr_state);
+        refpt = [lane(Idx,1) lane(Idx,2)];
+    else
+        %Find closest position on track to current location
+        Idx = knnsearch(cenpts', curr_pt);
+        %look up change in heading using same index
+        matchpt = lpinp(1, Idx);
+        %poseerror = angdiff(curr_state(4),ref_heading(Idx));
+        cenpts = cenpts';
+        %compute which side of center using cross product and
+        %apply proportional steering correction
+        whichside = whichwayoffcenter(Idx, cenpts, curr_state);
+        refpt = [cenpts(Idx,1) cenpts(Idx,2)];
+    end
+%     %Find closest position on track to current location
+%     Idx = knnsearch(cenpts', curr_pt);
+%     %look up change in heading using same index
+%     matchpt = lpinp(1, Idx);
+%     
+%     poseerror = ang_diff(curr_state(4),ref_heading(Idx));
+%     cenpts = cenpts';
+%     
+%     %compute which side of center using cross product and
+%     %apply proportional steering correction
+%     whichside = whichwayoffcenter(Idx, cenpts, curr_state);
+%     refpt = [cenpts(Idx,1) cenpts(Idx,2)];
 
     howfar = norm(refpt - curr_pt);
 
@@ -183,32 +277,138 @@ function [move, accumlated_poserr] = steering(lpinp, ref_track, curr_state,lasto
     end
 
     move = output;
+end
+function [move, accumlated_poserr] = steering(lpinp, ref_track, curr_state,lastoutput,outputsteps,accumlated_poserr)
+    
+    maxPinput = 0.07;
+    maxsteeringout = .15;
+    maxsteeringRateOfChange = .07;
+    max_accumerrorlimit = 5;
+    Igain = 0;%-.00001;
+    desire_speed = 5;
+    pGainoffCenter = .1;
+    Pdeadband = 1;
+    Ideadband = 1;
+    output = zeros(outputsteps,3);
+    output(:,2) = 90;
+
+    curr_state = curr_state';
+    cenpts = ref_track.cline;
+    right = ref_track.br;
+    left = ref_track.bl;
+    
+    % construct centerlines for left and right lane
+    c_right = (right + cenpts)/2;
+    c_left = (left+cenpts)/2;
+    ref_heading = ref_track.theta;
+    curr_pt = [curr_state(1), curr_state(3)];
+    
+    %Find closest position on track to current location
+    Idx = knnsearch(cenpts', curr_pt);
+    %look up change in heading using same index
+    matchpt = lpinp(1, Idx);
+    %poseerror = angdiff(curr_state(4),ref_heading(Idx));
+    cenpts = cenpts';
+    %compute which side of center using cross product and
+    %apply proportional steering correction
+    whichside = whichwayoffcenter(Idx, cenpts, curr_state);
+    refpt = [cenpts(Idx,1) cenpts(Idx,2)];
+%     end
+%     %Find closest position on track to current location
+%     Idx = knnsearch(cenpts', curr_pt);
+%     %look up change in heading using same index
+%     matchpt = lpinp(1, Idx);
+%     
+%     poseerror = ang_diff(curr_state(4),ref_heading(Idx));
+%     cenpts = cenpts';
+%     
+%     %compute which side of center using cross product and
+%     %apply proportional steering correction
+%     whichside = whichwayoffcenter(Idx, cenpts, curr_state);
+%     refpt = [cenpts(Idx,1) cenpts(Idx,2)];
+
+    howfar = norm(refpt - curr_pt); %euc dists from pt to reference
+
+    %proportional centerline control
+    if abs(howfar) > Pdeadband %if outside of deadband 
+        Pgain = -sign(whichside(3)) * howfar * pGainoffCenter; %calculate pgain 
+        if abs(Pgain) > maxPinput % clamp 
+            Pgain = sign(Pgain) * maxPinput;
+        end
+    else
+        Pgain = 0;
+    end
+
+    %integral centerline control
+%     accumlated_poserr = sign(whichside(3))* abs(howfar) + accumlated_poserr;
+
+    if howfar > Ideadband
+        accumlated_poserr = sign(whichside(3))* abs(howfar) + accumlated_poserr;
+    end
+
+    
+    %protect for windup
+    % if you cross over center reset error to recorrect 
+    if abs(accumlated_poserr) >= max_accumerrorlimit & sign(whichside(3)) ~= sign(accumlated_poserr)
+        accumlated_poserr = 0;
+    end
+
+
+
+    Igain = accumlated_poserr * Igain;
+
+    %proportional speed control (only decrease force from baseline if exceed desired speed)
+    if curr_state(2) > desire_speed
+        output(:,2) = lastoutput(end,2)*.9;
+%     elseif curr_state(2) <= desire_speed
+%         output(:,2) = lastoutput(end,2)*1.1; 
+    end
+
+
+    %% FOR SOME REASON LEFT AND RIGHT ARE FLIPPED RIGHT NOW SO DEAL WITH IT
+    
+    output = steeringrule(matchpt, output);
+
+    output(:,1) = output(:,1) + Pgain + Igain;
+
+    %rate limit
+    for i = 1:length(output)
+        outrate = lastoutput(end,1) - output(1,1);
+    if abs(outrate) > maxsteeringRateOfChange
+        output(i,1) = lastoutput(end,1) + sign(output(i,1))*maxsteeringRateOfChange;
+        output(i,1) = sign(output(i,1)) * min(abs(output(i,1)),maxsteeringout);
+        lastoutput = output(i,:);
+    end
+    end
+
+    move = output;
     
 end
 
-function whichside = whichwayoffcenter(Idx, cenpts, curr_state)
+function whichside = whichwayoffcenter(Idx, cenpts, curr_state) %cross product of heading x centerline to determine side you're on
 if Idx <= length(cenpts) -1
-    myposline = [curr_state(1)-cenpts(Idx,1), curr_state(3) - cenpts(Idx,2),0];
-    trackline = [cenpts(Idx+1,1)- cenpts(Idx,1), cenpts(Idx+1,2) - cenpts(Idx,2),0]; 
-    whichside = cross(trackline,myposline);
+    myposline = [curr_state(1)-cenpts(Idx,1), curr_state(3) - cenpts(Idx,2),0]; % unit vector from car to center line point 
+    trackline = [cenpts(Idx+1,1)- cenpts(Idx,1), cenpts(Idx+1,2) - cenpts(Idx,2),0]; % unit vector between curr pt and next point on track
+    whichside = cross(trackline,myposline); % z dim of x-product tells if you are on left or right (-/+) 
 else
-    whichside = 0;
+    whichside = [0 0 0];
 end
 end
 
 function output = steeringrule(matchpt, output)
-
+%derivative threshholds
 s_l = -0.02;
 %slight right
 s_r = 0.02; 
 %hard left
 h_l = -0.1;
 %hard right
-h_r = 0.1; 
+h_r = 0.1;
 
+%deltas 
 hard_turn = .06;
 soft_turn = 0.0175;
-
+%second(3rd element)output is for plotting which rule was chosen 
 if(matchpt < h_l)
     %turn hard left
     output(:,1) = -hard_turn;
